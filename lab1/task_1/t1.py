712b3f7978acc0ee97ea0fd9e5f3f3df751549f1
Daniel <55478231+Striz-lab@users.noreply.github.com>
2021-02-12 23:00:20 +0300

    fixed Torus

--- lab1/task_1/t1.py
+++ lab1/task_1/t1.py
@@ -16,6 +16,16 @@ l = gmsh.model.geo.addSurfaceLoop([s[i][1] for i in range(len(s))])
 gmsh.model.geo.addVolume([l])
 
 gmsh.model.geo.synchronize()
+#gmsh.model.geo.synchronize()
+
+# We specify element sizes imposed by a size field, just because we can :-)
+funny = False
+f = gmsh.model.mesh.field.add("MathEval")
+if funny:
+    gmsh.model.mesh.field.setString(f, "F", "2*Sin((x+y)/5) + 6")
+else:
+    gmsh.model.mesh.field.setString(f, "F", "11.2")
+gmsh.model.mesh.field.setAsBackgroundMesh(f)
 
 gmsh.model.mesh.generate(3)
 gmsh.write('Torus.msh')
